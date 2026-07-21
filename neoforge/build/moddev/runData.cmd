301367f80e004800abb5295e211fb31df4000d08
LB2345 <163643124+LovelyBunny11@users.noreply.github.com>
2026-07-21 21:45:02 +0300

    Initial Commit

--- neoforge/build/moddev/runData.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-for /f "tokens=2 delims=:." %%x in ('chcp') do set _codepage=%%x
-chcp 65001>nul
-cd C:\Users\Пользователь\Documents\Endemeral\neoforge\run
-C:\Users\Пользователь\.jdks\openjdk-21.0.2\bin\java.exe @C:\Users\Пользователь\Documents\Endemeral\neoforge\build\moddev\dataRunClasspath.txt @C:\Users\Пользователь\Documents\Endemeral\neoforge\build\moddev\dataRunVmArgs.txt -Dfml.modFolders=endemeral%%%%C:\Users\Пользователь\Documents\Endemeral\neoforge\build\classes\java\main;endemeral%%%%C:\Users\Пользователь\Documents\Endemeral\neoforge\build\resources\main net.neoforged.devlaunch.Main @C:\Users\Пользователь\Documents\Endemeral\neoforge\build\moddev\dataRunProgramArgs.txt
-if not ERRORLEVEL 0 (  echo Minecraft failed with exit code %ERRORLEVEL%  pause)
-chcp %_codepage%>nul
-endlocal
